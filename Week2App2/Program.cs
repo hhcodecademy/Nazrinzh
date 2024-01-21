02812da610e99f65cb5c1b208e2c303aeaa19fe5
Huseyn Hasanli <huseyn.hasanli@code.edu.az>
2024-01-21 19:47:28 +0400

    tets

---
+++ Week2App2/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Week2App2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello! Welcome to my project");
+            Console.WriteLine(" Day number for result ");
+
+            int daynumber =int.Parse( Console.ReadLine());
+            if (daynumber == 1) {
+                Console.WriteLine("Watching film");
+
+            }
+            else if (daynumber == 2)
+            {
+                Console.WriteLine("go to sport");
+            }
+            else if (daynumber == 3) {
+                Console.WriteLine("do assigment");
+
+            }
+            else if (daynumber == 4)
+            {
+                Console.WriteLine("go shopping");
+
+            }
+            else if (daynumber == 5)
+            {
+                Console.WriteLine("sleeping");
+
+            }
+            else if (daynumber == 6)
+            {
+                Console.WriteLine("go to Code Academy");
+
+            }
+            else if (daynumber == 7)
+            {
+                Console.WriteLine("go to academy");
+
+            }
+            else {
+                Console.WriteLine("please enter correct day");
+                    }
+
+        }
+    }
+}
